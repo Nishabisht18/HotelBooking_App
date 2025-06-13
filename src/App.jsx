56a275e1c64a592c276d42db748fed8be4aff852
Nisha Bisht <nishabisht4002@gmail.com>
2025-06-13 13:08:46 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './components/Pages/Home';
+import Footer from './components/Footer';
+import AllRooms from './components/Pages/AllRooms';
+import RoomDetails from './components/Pages/RoomDetails';
+import MyBooking from './components/Pages/MyBooking';
+import HotelReg from './components/HotelReg';
+import Layout from './components/Pages/HotelOwner/Layout';
+import Dashboard from './components/Pages/HotelOwner/Dashboard';
+import AddRoom from './components/Pages/HotelOwner/AddRoom';
+import ListRoom from './components/Pages/HotelOwner/ListRoom';
+
+const App = () => {
+    const isOwnerPath=useLocation().pathname.includes("owner");
+  return (
+    <div>
+        {!isOwnerPath && <Navbar/>}
+        
+        {false && <HotelReg/>}
+        <div className='min-h-[70vh]'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/rooms' element={<AllRooms/>}/>
+            <Route path='/rooms/:id' element={<RoomDetails/>}/>
+            <Route path='/my-bookings' element={<MyBooking/>}/>
+            <Route path='/owner' element={<Layout/>}>
+              <Route index element={<Dashboard/>}/>  
+              <Route path='list-room' element={<ListRoom/>}/>  
+              <Route path='add-room' element={<AddRoom/>}/>              
+            </Route>
+          </Routes>
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../Hero'
+import FeaturedDestination from '../FeaturedDestination'
+import ExclusiveOffers from '../ExclusiveOffers'
+import Testimontial from '../Testimontial'
+import NewsLetter from '../NewsLetter'
+import Footer from '../Footer'
+
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <FeaturedDestination/>
+        <ExclusiveOffers/>
+        <Testimontial/>
+        <NewsLetter/>
+    </>
+  )
+}
+
+export default Home
